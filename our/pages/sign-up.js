e425c366704e26f3bb459e4c68bf3fad6b7037e2
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-31 11:22:03 +0700

    change name input email

--- our/pages/sign-up.js
+++ our/pages/sign-up.js
@@ -72,7 +72,7 @@ const SignUp = () => {
                   </div>
                   <div className='w-[25rem] border-b-2 md:border-b-0'>
                     <label>Email Adress :</label><br />
-                    <Field type='text' name='email' placeholder='Enter your email adress' className='input md:input-bordered bg-transparent focus:outline-none mt-2 w-full rounded-2xl'/>
+                    <Field type='email' name='email' placeholder='Enter your email adress' className='input md:input-bordered bg-transparent focus:outline-none mt-2 w-full rounded-2xl'/>
                     {errors.email && touched.email ? <div className='absolute text-red-500 text-sm'>{errors.email}</div> : null}
                   </div>
                   <div className='w-[25rem] relative border-b-2 md:border-b-0'>
