56ff1950f580643ed39e40e337d380b2727ee3b8
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-31 07:25:28 +0700

    implement input validation and responsive mobile

--- our/pages/sign-up.js
+++ our/pages/sign-up.js
@@ -1,21 +1,52 @@
+import React from 'react'
 import Image from 'next/image'
 import Logo from '../images/logoOurCoffee.png'
 import GoogleLogo from '../images/google.png'
 import FeatherIcon from 'feather-icons-react'
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
+import YupPasword from 'yup-password'
+YupPasword(Yup)
+
+// const phoneRegExpID = /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/
+const phoneRegExpID = /^(^08)(\d{8,10})$/
+
+const SignUpSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .password()
+    .min(8, 'Min lenght 8')
+    .minLowercase(1, 'Min lowercase 1')
+    .minUppercase(1, 'Min uppercase 1')
+    .minSymbols(1, 'Min symbol 1')
+    .minNumbers(1, 'Min number 1'),
+  phoneNumber: Yup.string()
+    .matches(phoneRegExpID, 'Invalid phone number')
+    .required('Required')
+})
 
 const SignUp = () => {
+  const [eyeClicked, setEyeClicked] = React.useState(false)
+  const showPassword = () => {
+    if (eyeClicked === false) {
+      setEyeClicked(true)
+    } else {
+      setEyeClicked(false)
+    }
+  }
+
   return(
     <>
       <section>
         <div className='flex'>
           {/* Left */}
-          <div className='flex-[45%]'>
+          <div className='hidden md:block flex-[45%]'>
             <Image src={require('../images/authBackground.png')} alt='Background' className='5/6'/>
           </div>
 
           {/* Right */}
-          <div className='flex-[55%]'>
-            <div className='flex items-center py-14 pl-12 pr-36'>
+          <div className='flex-[55%] py-10 md:py-0 bg-[#F8EDE3] md:bg-white'>
+            <div className='hidden md:flex items-center py-14 pl-12 pr-36'>
               <div className='flex-1 flex items-center gap-3'>
                 <Image src={Logo} alt='Logo' className='w-10 h-10' />
                 <p className='font-bold text-2xl'>Our Coffee</p>
@@ -24,36 +55,55 @@ const SignUp = () => {
                 <button className='btn btn-sm bg-[#7D6E83] rounded-3xl font-bold text-white'>Login</button>
               </div>
             </div>
-            <form className='flex flex-col items-center gap-5'>
-              <div className='py-5'>
-                <h1 className='font-bold text-3xl text-[#7D6E83]'>Sign Up</h1>
-              </div>
-              <div className='w-[25rem]'>
-                <label>Email Adress :</label><br />
-                <input type='text' name='email' placeholder='Enter your email adress' className='input input-bordered focus:outline-none mt-2 w-full rounded-2xl'/>
-              </div>
-              <div className='w-[25rem]'>
-                <label>Password :</label><br />
-                <input type='password' name='password' placeholder='Enter your password' className='input input-bordered focus:outline-none mt-2 w-full rounded-2xl'/>
-              </div>
-              <div className='w-[25rem]'>
-                <label>Phone Number :</label><br />
-                <input type='phoneNumber' name='phoneNumber' placeholder='Enter your phone number' className='input input-bordered focus:outline-none mt-2 w-full rounded-2xl'/>
-              </div>
-              <div className='w-[25rem] mt-3'>
-                <button className='btn bg-[#7D6E83] text-white font-bold text-lg rounded-2xl'>Sign Up</button>
-              </div>
+            <div className='flex justify-center'>
+            <Formik initialValues={{
+              email: '',
+              password: '',
+              phoneNumber: ''
+            }}
+            validationSchema={SignUpSchema}
+            onSubmit={(value) => console.log(value)}>
+              {({errors, touched}) => (
+                <Form className='px-5 flex flex-col items-center gap-8'>
+                  <div className='py-5'>
+                    <Image src={require('../images/girlChilling.png')} className='md:hidden' />
+                    <h1 className='hidden md:block font-bold text-3xl text-[#7D6E83]'>Sign Up</h1>
+                  </div>
+                  <div className='w-[25rem] border-b-2 md:border-b-0'>
+                    <label>Email Adress :</label><br />
+                    <Field type='text' name='email' placeholder='Enter your email adress' className='input md:input-bordered bg-transparent focus:outline-none mt-2 w-full rounded-2xl'/>
+                    {errors.email && touched.email ? <div className='absolute text-red-500 text-sm'>{errors.email}</div> : null}
+                  </div>
+                  <div className='w-[25rem] relative border-b-2 md:border-b-0'>
+                    <label>Password :</label><br />
+                    <FeatherIcon onClick={showPassword} icon={eyeClicked ? 'eye-off' : 'eye'} className='absolute right-5 bottom-3 opacity-20 cursor-pointer'/>
+                    <Field type={eyeClicked? 'text' : 'password'} name='password' placeholder='Enter your password' className='input md:input-bordered bg-transparent focus:outline-none mt-2 w-full rounded-2xl'/>
+                    {errors.password && touched.password ? <div className='text-red-500 text-sm absolute'>{errors.password}</div> : null}
+                  </div>
+                  <div className='w-[25rem] border-b-2 md:border-b-0'>
+                    <label>Phone Number :</label><br />
+                    <Field type='phoneNumber' name='phoneNumber' placeholder='Enter your phone number' className='input md:input-bordered bg-transparent focus:outline-none mt-2 w-full rounded-2xl'/>
+                    {errors.phoneNumber && touched.phoneNumber ? <div className='absolute text-red-500 text-sm'>{errors.phoneNumber}</div> : null}
+                  </div>
+                  <div className='w-[25rem] mt-3'>
+                    <button type='submit' className='btn bg-[#7D6E83] text-white font-bold text-lg rounded-2xl'>Sign Up</button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+            </div>
+            <div className='flex justify-center py-5'>
               <div className='w-[25rem]'>
                 <button className='btn bg-white drop-shadow-xl text-[#7D6E83] border-none font-bold text-lg rounded-2xl flex items-center gap-5 hover:text-white'>
-                  <Image src={GoogleLogo} />
+                  <Image src={GoogleLogo} alt='Google Logo' />
                   <p>Sign up with Google</p>
                 </button>
               </div>
-            </form>
+            </div>
           </div>
         </div>
 
-        <footer className='flex gap-20 relative bg-[#F8EDE3] pt-36 pb-20 pl-28 px-52'>
+        <footer className='hidden md:flex gap-20 relative bg-[#F8EDE3] pt-36 pb-20 pl-28 px-52'>
           <div className='absolute -top-24 inset-x-28 bg-white shadow-lg rounded-lg flex items-center p-10 w-3/4'>
             <div className='flex-1'>
               <p className='font-bold text-3xl w-2/4 mb-5'>Get your member card now!</p>
