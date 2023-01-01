53d37fb87830e0d790b97a8e1ec7ff8e03dac7d3
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-01-01 17:44:23 +0700

    create manage orders page

---
+++ our/pages/tes.js
@@ -0,0 +1,60 @@
+<div className="bg-[#FFFFFF] rounded-lg">
+<div>
+  <div>Delivery Order</div>
+  <div className="flex gap-2">
+    <div>For</div>
+    <div>Zulaikha</div>
+  </div>
+</div>
+
+<div className="flex">
+  <div>
+    <Image src={require("../images/hazelnut.png")} width="90" height="82" className="rounded-lg" alt="desc"></Image>
+  </div>
+  <div>
+    <div>Hazelnut Latte</div>
+    <div>x1</div>
+    <div>Regular</div>
+  </div>
+  <div>
+    <div>24.0</div>
+  </div>
+</div>
+
+<div className="flex">
+  <div>
+    <Image src={require("../images/chikenFW.png")} width="90" height="82" className="rounded-lg" alt="desc"></Image>
+  </div>
+  <div>
+    <div>Chicken Fire Wings</div>
+    <div>x2</div>
+    <div></div>
+  </div>
+  <div>
+    <div>30.0</div>
+  </div>
+</div>
+
+<div className="outline outline-1 outline-[#D0B8A8]"></div>
+<div>
+  <div className="flex">
+    <div className="grow">SUBTOTAL</div>
+    <div>IDR 120.000</div>
+  </div>
+  <div className="flex">
+    <div className="grow">TAX & FEES</div>
+    <div>IDR 20.000</div>
+  </div>
+  <div className="flex">
+    <div className="grow">SHIPPING</div>
+    <div>IDR 10.000</div>
+  </div>
+</div>
+
+<div>
+<div className="flex">
+    <div className="grow">TOTAL</div>
+    <div>IDR 150.000</div>
+  </div>
+</div>
+</div>
