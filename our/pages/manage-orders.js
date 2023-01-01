14079fb6908d32a1b2dbb5d4ee161c17f600a6d7
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-01-01 18:14:30 +0700

    fixing responsive manage orders

--- our/pages/manage-orders.js
+++ our/pages/manage-orders.js
@@ -9,17 +9,17 @@ const ManageOrders = () => {
       {/* Konten */}
       <div className="bg-[url('../assets/bg-1.png')] bg-cover bg-no-repeat font-rubik">
         <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="ml-[10%]">
+          <div className="flex flex-col justify-center items-center ml-[10%] pt-[10%]">
             <div className="font-bold text-white text-[40px] drop-shadow-lg">Finish your customer order now.</div>
             <div className="relative mt-[15%] bg-white w-[350px] h-[524px] rounded-lg drop-shadow-xl">
               <div className="absolute bg-white mt-[-10%] w-[350px] h-[524px] rounded-lg drop-shadow-xl">
                 <div className="absolute bg-white mt-[-10%] w-[350px] h-[524px] rounded-lg drop-shadow-xl flex flex-col">
                   <div className="text-center pt-[10%]">
-                    <div>Delivery Order</div>
+                    <div className="font-bold text-[35px]">Delivery Order</div>
                       <div>For Zulaikha</div>
                   </div>
 
-                  <div className="flex gap-[33px]">
+                  <div className="flex gap-[33px] px-[5%] mt-[5%]">
                     <div>
                       <Image src={require("../images/hazelnut.png")} width="90" height="82" className="rounded-lg" alt="desc"></Image>
                     </div>
@@ -33,7 +33,7 @@ const ManageOrders = () => {
                     </div>
                   </div>
 
-                  <div className="flex gap-[33px]">
+                  <div className="flex gap-[33px] px-[5%] mt-[5%]">
                     <div>
                       <Image src={require("../images/chikenFW.png")} width="90" height="82" className="rounded-lg" alt="desc"></Image>
                     </div>
@@ -47,8 +47,8 @@ const ManageOrders = () => {
                     </div>
                   </div>
 
-                  <div className="outline outline-1 outline-[#D0B8A8]"></div>
-                  <div>
+                  <div className="outline outline-1 outline-[#D0B8A8] mt-[5%]"></div>
+                  <div className="flex flex-col gap-4 px-[5%]">
                     <div className="flex">
                       <div className="grow">SUBTOTAL</div>
                       <div>IDR 120.000</div>
@@ -63,7 +63,7 @@ const ManageOrders = () => {
                     </div>
                   </div>
 
-                  <div>
+                  <div className="mt-[45px] px-[5%]">
                     <div className="flex">
                       <div className="grow">TOTAL</div>
                       <div>IDR 150.000</div>
@@ -72,15 +72,22 @@ const ManageOrders = () => {
                 </div>
               </div>
             </div>
+
+            <button className="flex bg-[#7D6E83] text-white gap-[38px] px-[5%] py-[22px] rounded-lg mt-[54px] mb-[10%]">
+              <div>Swipe up to see upcoming orders</div>
+              <div>
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+              </div>
+            </button>
           </div>
 
 
-          <div>
-            <div className="flex text-white w-[50%] pt-[20%]">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex text-white w-[60%] md:w-[50%] pt-[20%]">
               <div className="grow">Address Details</div>
               <div>edit</div>
             </div>
-            <div className="bg-white w-[50%] rounded-lg px-[42px] py-[30px]">
+            <div className="bg-white w-[60%] md:w-[50%] rounded-lg px-[42px] py-[30px]">
               <div>Delivery to Iskandar Street</div>
               <div className="outline outline-1 outline-[#DFD3C3]"></div>
               <div>Km 5 refinery road oppsite re
@@ -89,10 +96,10 @@ const ManageOrders = () => {
               <div>+62 81348287878</div>
             </div>
 
-            <div className="flex text-white w-[50%] pt-[10%]">
+            <div className="flex text-white w-[60%] md:w-[50%] pt-[10%]">
               <div className="grow">Payment Method</div>
             </div>
-            <div className="bg-white w-[50%] rounded-lg px-[42px] py-[30px]">
+            <div className="bg-white w-[60%] md:w-[50%] rounded-lg px-[42px] py-[30px]">
               <div>
                 <div className="flex items-center gap-[11px]">
                   <input type="radio" name="radio-1" className="radio" />
@@ -124,7 +131,7 @@ const ManageOrders = () => {
                 </div>
               </div>
             </div>
-            <button className="w-[50%] bg-[#DFD3C3] py-[27px] text-white rounded-lg mt-[47px]"> Mark as done</button>
+            <button className="w-[60%] md:w-[50%] bg-[#DFD3C3] py-[27px] rounded-lg mt-[47px] mb-[5%]"> Mark as done</button>
           </div>
         </div>
       </div>
