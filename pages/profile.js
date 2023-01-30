dbde474393f80fe0ce19c2b962c3e24574bd3291
ilhamdanu <86812576+ilhamdanu13@users.noreply.github.com>
2023-01-30 21:02:20 +0700

    add format upload

--- pages/profile.js
+++ pages/profile.js
@@ -17,6 +17,7 @@ const Profile = () => {
   const token = useSelector((state) => state?.auth?.token);
   const router = useRouter();
   const [picture, setpicture] = useState(false);
+  const [alertSize, setAlertSize] = useState(false);
   const [bio, setBio] = useState({});
   console.log(bio);
   useEffect(() => {
@@ -36,7 +37,8 @@ const Profile = () => {
     const file = e.target.picture.files[0];
     console.log(file);
     if (file?.size > 1024 * 1024 * 2) {
-      window.alert("File too large");
+      setAlertSize(true);
+      // window.alert("File too large");
     } else {
       try {
         const form = new FormData();
@@ -106,13 +108,40 @@ const Profile = () => {
                     </button>
                   </div>
                   <form onSubmit={upload}>
-                    <input type="file" name="picture" className="mb-5" />
+                    <input
+                      type="file"
+                      name="picture"
+                      accept="image/png, image/jpeg, image/jpg"
+                      className="mb-5"
+                    />
                     <button
                       type="submit"
-                      className="py-[17px] w-full rounded-[20px] bg-[#7d6e83] border-1 font-bold  text-white duration-300"
+                      className="py-[17px] w-full rounded-[20px] bg-[#7d6e83] border-1 font-bold  text-white duration-300 mb-5"
                     >
                       Save
                     </button>
+                    {alertSize ? (
+                      <div className="alert alert-warning shadow-lg">
+                        <div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="stroke-current flex-shrink-0 h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                            />
+                          </svg>
+                          <span>Warning: Cannot more than 2 MB</span>
+                        </div>
+                      </div>
+                    ) : (
+                      false
+                    )}
                   </form>
                 </ReactModal>
               </div>
