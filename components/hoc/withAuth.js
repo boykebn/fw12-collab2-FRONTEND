1173ced1eb918a586236ac597db3b882eebd9c51
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-30 14:04:27 +0700

    remove eslint

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { useSelector } from "react-redux"
-import { useRouter } from 'next/router'
-import jwt_decode from 'jwt-decode'
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import jwt_decode from "jwt-decode";
 
 const withAuth = (Components) => {
+  // eslint-disable-next-line react/display-name
   return (props) => {
-    const router = useRouter()
-    const token = useSelector((state) => state?.auth?.token)
-    let role
-    token ? {role} = jwt_decode(token) : false
-    React.useEffect(()=>{
-      if(!token) {
-        router.replace('/login')
-      } else if (token && role === '1') {
-        router.replace('/product')
+    const router = useRouter();
+    const token = useSelector((state) => state?.auth?.token);
+    let role;
+    token ? ({ role } = jwt_decode(token)) : false;
+    React.useEffect(() => {
+      if (!token) {
+        router.replace("/login");
+      } else if (token && role === "1") {
+        router.replace("/product");
       }
-    }, [token])
-    return <Components {...props} />
-  }
-}
+    }, [token]);
+    return <Components {...props} />;
+  };
+};
 
-export default withAuth
+export default withAuth;
